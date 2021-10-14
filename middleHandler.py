b1bd290ad766be6fd721f5d6855e28572adc35ef
bitcoder-17 <70048387+bitcoder-17@users.noreply.github.com>
2021-10-14 15:30:19 -0500

    N/A

--- middleHandler.py
+++ middleHandler.py
@@ -6,7 +6,7 @@ import sys
 #focusing on id and codename
 
 #Fetching credentials with json
-url_credentials = ''
+url_credentials = 'https://data.heroku.com/dataclips/vutwqwveweoamnjuykcracjwjyaw.json'
 
 data = requests.get(url_credentials).json()
 
