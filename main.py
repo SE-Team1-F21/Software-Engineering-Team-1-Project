2946d9da3b4b18c5e0433a050d74906a355dfb13
jmarti44 <jmarti1997.jm@gmail.com>
2021-11-03 12:13:10 -0500

    Completed button action and red flagging

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from flask import jsonify
 from flask.signals import request_started
 from middleHandler import connection
 
+#http://127.0.0.1:5000/ 
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -22,23 +24,45 @@ def game():
 def submit():
     if request.method == 'POST':
         #print ('route has been met?')
+        playerInfo = []
         dataResult = request.get_json(force=True)
-        print (type(dataResult))
+        for i in dataResult.keys():
+            for j in dataResult.values():
+                playerInfo.append(i)
+                playerInfo.append(j)
+        playerInfo.append('R')
+        print(playerInfo)
+  
+
+        ##playerInfo contains the following
+        #[id,name,team_color]
+
+
         for id, codeName in dataResult.items():
             connection(id, codeName)
         dataReply = {'this':'works'}
         return jsonify(dataReply)
     
-    
-@app.route('/red_submit', methods = ['GET','POST'])
+##To be worked on
+@app.route('/green_submit', methods = ['GET','POST'])
 def submit_2():
     if request.method == 'POST':
-        print ('route has been met?')
-        dataResult = request.get_json(force=True)
-        #print (dataResult)
-
-        dataReply = {'this':'works'}
+        # playerInfo = []
+        # print ('route has been met?')
+        # dataResult = request.get_json(force=True)
+        # #print (dataResult)
+        # for i in dataResult.keys():
+        #     for j in dataResult.values():
+        #         playerInfo.append(i)
+        #         playerInfo.append(j)
+        # playerInfo.append('G')
+        # print(playerInfo)
+        # dataReply = {'this':'works'}
         return jsonify(dataReply)
+
+@app.route('/action',methods=['GET', 'POST'])
+def playerAction():
+    return render_template('playAction.html')
 
 
 
