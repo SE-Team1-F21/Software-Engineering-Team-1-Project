71484f0d5fc55c2cdf83c8e5cc59332c26652bc6
Carson-Reed <90006626+Carson-Reed@users.noreply.github.com>
2021-11-26 11:06:42 -0600

    Add files via upload

--- python_trafficgenerator.py
+++ python_trafficgenerator.py
@@ -6,26 +6,11 @@ bufferSize  = 1024
 serverAddressPort   = ("127.0.0.1", 7501)
 
 
-# print('this program will generate some test traffic for 2 players on the red ')
-# print('team as well as 2 players on the green team')
-# print('')
-
 with open('./files/rednames.txt') as f:
 	redTeam = f.readlines()[0].split(',')
 with open('./files/greennames.txt') as f:
 	greenTeam = f.readlines()[0].split(',')
 
-
-
-#red1 = input('Enter id of red player 1 ==> ')
-#red2 = input('Enter id of red player 2 ==> ')
-#green1 = input('Enter id of green player 1 ==> ')
-#green2 = input('Enter id of green player 2 ==> ')
-
-
-
-#print('')
-#counter = input('How many events do you want ==> ')
 counter = 100
 
 # Create datagram socket
@@ -33,26 +18,9 @@ UDPClientSocketTransmit = socket.socket(family=socket.AF_INET, type=socket.SOCK_
 
 # counter number of events, random player and order
 i = 1
-while i < int(counter):
-	# if random.randint(1,len(redTeam)) == 1:
-	# 	redplayer = red1
-	# else:
-	# 	redplayer = red2
 
-	# if random.randint(1,2) == 1:
-	# 	greenplayer = green1
-	# else: 
-	# 	greenplayer = green2	
-
-	# if random.randint(1,2) == 1:
-	# 	message = redplayer + ":" + greenplayer
-	# else:
-	# 	message = greenplayer + ":" + redplayer
-
-	# print(message)
-	# i+=1;
-	# UDPClientSocketTransmit.sendto(str.encode(str(message)), serverAddressPort)
-	# time.sleep(random.randint(1,3))
+#time.sleep(30) #could use to wait the 30 seconds on the timer
+while i < int(counter): #could set time.sleep to 1 second each time and then set counter to 6 minutes in seconds
 
 	if random.randint(1,2) == 1:
 		message = "r" + ":" + str(random.randint(1,len(redTeam)-1)) + ":" + str(random.randint(1,len(greenTeam)-1))
